8c9c132b42671f5df54c1acb3a63afc1497f94a5
+ <+>
2022-08-22 11:09:36 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(num1,num2){
+ num1=2000;
+ num2=100;
+ return num1/num2;
+}
+const square=(a=2)=>{
+    return a**2;
+
+}
+const add=(num3,num4)=>{
+    num3=3;
+    num4=4;
+return num3+num4;
+}
